60b5b3d56d6654783617232a3986a79312bfb3e3
viacheslav <kabaniets1990@gmail.com>
2024-06-17 12:57:35 +0300

    init frontend part

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home';
+import LoginPage from './components/auth/login';
 import './App.css';
+import PrivateRouter from './utils/router/privateRouter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route element={<PrivateRouter />}>
+          <Route path='' element={<Home />} />
+        </Route>
+        <Route path='login' element={<LoginPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/utils/router/privateRouter.tsx
@@ -0,0 +1,8 @@
+import { Navigate, Outlet } from "react-router-dom"
+
+const PrivateRouter = () => {
+    const auth = true;
+    return auth ? <Outlet /> : <Navigate to="login" />
+}
+
+export default PrivateRouter
