332a101029b0de009ccfcedf572944cc2f437519
viacheslav <kabaniets1990@gmail.com>
2024-06-25 17:16:51 +0300

    improve router-logic using outlet

---
+++ src/components/layout/styles.ts
@@ -0,0 +1,10 @@
+import { makeStyles } from "@mui/styles";
+
+export const useStyles = makeStyles({
+  mainSection: {
+    display: "flex",
+    flexGrow: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+});
