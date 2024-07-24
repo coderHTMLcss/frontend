ad383e7c4fd238465555dce1e487f6ad266399e5
viacheslav <kabaniets1990@gmail.com>
2024-07-24 14:45:30 +0300

    add logout logic

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,18 +1,46 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { useAppDispatch, useAppSelector } from '../../utils/hook'
 import { getNews } from '../../store/thunks/news';
 import { Box, Grid, Link, Typography } from '@mui/material';
 import { StyledRoot, StyledBlockTitle, StyledNewsBlock, StyledNewsTitle, StyledReadMore } from './styles';
 
 const NewsPage: FC = (): JSX.Element => {
+    const [newsItem, setNewsItem] = useState([]);
+    const [offset, setOffset] = useState([0, 10]);
     const dispatch = useAppDispatch();
-    const { news } = useAppSelector((state) => state.news)
+    const { news } = useAppSelector((state) => state.news);
+
+    const handleChangeOffset = (offset: any) => {
+        setOffset([offset[0], offset[1] + 10]);
+    };
 
     useEffect(() => {
-        dispatch(getNews())
-    }, [dispatch])
+        setNewsItem(news.slice(offset[0], offset[1]));
+    }, [news, offset]);
 
-    const renderNewsBlock = news.map((element: any) => (
+    useEffect(() => {
+        dispatch(getNews());
+    }, [dispatch]);
+
+    useEffect(() => {
+        const handleScroll = (e: any) => {
+            if (
+                e.target.documentElement.scrollHeight -
+                (e.target.documentElement.scrollTop + window.innerHeight) <
+                100
+            ) {
+                handleChangeOffset(offset);
+            }
+        };
+
+        document.addEventListener('scroll', handleScroll);
+        return () => {
+            document.removeEventListener('scroll', handleScroll);
+        };
+    }, [offset]);
+
+
+    const renderNewsBlock = newsItem.map((element: any) => (
         <StyledNewsBlock container key={element.id}>
             <Grid item xs={12} md={3}>
                 <img src={element.imageurl} alt={element.category} />
